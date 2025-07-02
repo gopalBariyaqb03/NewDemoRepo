44e8bf2732c0e9ff88f6008c391d62ca199db34c
gopalBariyaqb03 <gopalbariyaqb03@gmail.com>
2025-07-02 13:48:25 +0530

    update chemist

--- src/test/java/Tests/AdminTest.java
+++ src/test/java/Tests/AdminTest.java
@@ -69,6 +69,7 @@ public class AdminTest extends BasePage {
 
     @Test
     public void verifyUpdateChemistScenario(){
+        common.logPrint("test");
         loginWithMRCredential();
         String chemistName = adminPage.createChemistAndApprove();
         adminPage.verifyEntityIsCreatedIsShowingInTheTableAndItStatusIsPending(chemistName);
