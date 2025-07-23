09ad3a3a417cf31c76dc62a3c5f53c3948d6e390
QB-Keyur <keyur@quantumbot.co.in>
2025-07-23 16:42:29 +0530

    Added all the Inventory edit scenarios

--- src/test/java/Utils/Common.java
+++ src/test/java/Utils/Common.java
@@ -1863,6 +1863,14 @@ public class Common extends Locators {
 
         actions.sendKeys(Keys.DOWN).perform();
     }
+    public void updatedSuccessfullyValidationLog(String module_name, String prev_value, String cur_value) {
+
+        if (!prev_value.equals(cur_value)) {
+            System.out.println(module_name + " Updated Successfully from " + prev_value + " To " + cur_value);
+        } else {
+            System.out.println("Error while updating " + prev_value + " To " + cur_value);
+        }
+    }
 }
 
 
